69b9e2fea50b9337fdb9cfe902576b7b1198c7d4
nasser <nasser.elidrissi065@gmail.com>
2016-03-26 19:52:19 +0000

    add header navbar

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
+var Header = require('./header');
+
+var renderCount= 0;
 
 module.exports = React.createClass({
   render: function(){
+    console.log(renderCount++);
     return <div>
-      I am a header
+      <Header />
       {this.props.children}
     </div>
   }
